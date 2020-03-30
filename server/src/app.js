868128b56d47c248859dd606d590a4064639428f
German <germanvillapa9@gmail.com>
2020-03-29 18:32:55 -0600

    Conexion con firebase y base de signup

--- server/src/app.js
+++ server/src/app.js
@@ -8,6 +8,21 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+// Firebase integration.
+// Seguro hay una mejor manera de hacerlo, poniendolo en otro archivo o algo.
+// Despues vemos
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./proyectohospitales-f1287-firebase-adminsdk-r36by-29ba8608e2.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://proyectohospitales-f1287.firebaseio.com"
+});
+
+let db = admin.firestore();
+
 /*
 //////
 // Ejemplo de un endpoint.
@@ -15,6 +30,30 @@ app.use(cors())
 */
 
 app.post('/register', (req, res) => {
+  //Creamos la conexion a la tabla 'usuarios'
+  let usersTable = db.collection('usuarios')
+  var statusCode = 0
+
+  // Agregamos un documento.
+  usersTable.add({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    is_active: true,
+    date_joined: Date.now()
+  }).then((status) => {
+      statusCode = status
+  }).catch((err) => {
+      statusCode = err
+  })
+
+  res.send({
+    statusCode: statusCode,
+    message: `${req.body.name}, ya te registraste campeon`
+  })
+})
+
+app.get('/pacientes', (req, res) => {
   res.send({
     message: `${req.body.name}, ya te registraste campeon`
   })
