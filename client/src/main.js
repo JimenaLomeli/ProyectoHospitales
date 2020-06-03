69cf8dc4d40488c115991b628317bed4eb90b1dc
laurasantacruz <smlaura97@gmail.com>
2020-06-03 10:06:35 -0500

    Card con mapa

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+require("dotenv").config();
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -10,6 +11,19 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import colors from 'vuetify/lib/util/colors'
 
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+var apiKey = require('./api_map_key.json')
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: apiKey["VUE_APP_MY_VARIABLE"],
+  },
+  installComponents: false
+})
+
+Vue.component('google-map', VueGoogleMaps.Map);
+// Vue.component('GmapMarker', VueGoogleMaps.GmapMarker);
 
 Vue.use(VueTextareaAutosize)
 
