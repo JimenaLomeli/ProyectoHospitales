7f2a0473aa7f2f2e69331d1c5466d706f20fd358
German <germanvillapa9@gmail.com>
2020-06-07 19:54:39 -0500

    a

--- server/src/app.js
+++ server/src/app.js
@@ -11,12 +11,7 @@ const app = express()
 app.use(morgan('combine'))
 app.use(bodyParser.json())
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
+app.use(cors())
 
 /*
 //////
