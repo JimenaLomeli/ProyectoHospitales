29eb7fad8f4d8377c91dbd4c502fffda1d507ced
German <germanvillapa9@gmail.com>
2020-06-07 19:30:39 -0500

    a

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const morgan = require('morgan')
 const serviceAccount = require('./firebaseConfig')
 var firebase = require('firebase');
@@ -10,17 +11,7 @@ const app = express()
 app.use(morgan('combine'))
 app.use(bodyParser.json())
 
-function cors(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  if (req.method === "OPTIONS") {
-    return res.send(204);
-  }
-  next();
-}
-
-app.use(cors)
+app.use(cors())
 
 /*
 //////
